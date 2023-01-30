a2a8b778d24b8c82a20adf0f428c97d0b5be5dc6
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2023-01-30 08:51:14 +0100

    Update EmployeeDAOImpl.java

--- spring/src/main/java/com/example/spring/JDBC/EmployeeDAOImpl.java
+++ spring/src/main/java/com/example/spring/JDBC/EmployeeDAOImpl.java
@@ -1,8 +1,10 @@
 package com.example.spring.JDBC;
 
 import com.example.spring.DAO.EmployeeDAO;
+import com.example.spring.mapper.EmployeeRowMapper;
 import com.example.spring.modell.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -17,48 +19,55 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     private JdbcTemplate jdbcTemplate;
 
     public void createEmployee(Employee employee) {
-        String SQL = "insert into employee (firstname, lastname, email, password) values (?, ?, ?, ?)";
+        String SQL = "insert into employee (firstName, lastName, email, password) values (?, ?, ?, ?)";
         jdbcTemplate.update(SQL, employee.getFirstName(), employee.getLastName(), employee.getEmail(),
                 employee.getPassword());
     }
-    public Employee getEmployeeById(Long id) {
+    public Employee getEmployeeById(int id) {
         String SQL = "select * from employee where id = ?";
-        Employee employee = jdbcTemplate.queryForObject(SQL, new Object[]{id}, new EmployeeMapper());
+        Employee employee = jdbcTemplate.queryForObject(SQL, new Object[]{id}, new EmployeeRowMapper());
         return employee;
     }
     public List<Employee> getAllEmployees() {
         String SQL = "select * from employee";
-        List<Employee> employees = jdbcTemplate.query(SQL, new EmployeeMapper());
+        List<Employee> employees = jdbcTemplate.query(SQL, new EmployeeRowMapper());
         return employees;
     }
     public void updateEmployee(Employee employee) {
-        String SQL = "update employee set firstname = ?, lastname = ?, email = ?, password = ? where id = ?";
+        String SQL = "update employee set firstName = ?, lastName = ?, email = ?, password = ? where id = ?";
         jdbcTemplate.update(SQL, employee.getFirstName(), employee.getLastName(), employee.getEmail(), employee.getPassword(), employee.getId());
     }
-    public void deleteEmployee(Long id) {
+    public void deleteEmployee(int id) {
         String SQL = "delete from employee where id = ?";
         jdbcTemplate.update(SQL, id);
     }
 
     @Override
     public Employee getEmployeeByEmailAndPassword(String email, String password) {
-        return null;
+        try {
+            String sql = "SELECT * FROM employee WHERE email = ? AND password = ?";
+            Employee employee=jdbcTemplate.queryForObject(sql,
+                    new Object[] { email, password }, new EmployeeRowMapper());
+            return employee;
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
-    private final class EmployeeMapper implements RowMapper<Employee> {
+    }
+
+   /* private final class EmployeeMapper implements RowMapper<Employee> {
         public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
             Employee employee = new Employee();
             employee.setId(rs.getLong("id"));
-            employee.setFirstName(rs.getString("firstname"));
-            employee.setLastName(rs.getString("lastname"));
+            employee.setFirstName(rs.getString("firstName"));
+            employee.setLastName(rs.getString("lastName"));
             employee.setEmail(rs.getString("email"));
             employee.setPassword(rs.getString("password"));
             return employee;
         }
 
 
-    }
-}
-
+    }*/
 
 
