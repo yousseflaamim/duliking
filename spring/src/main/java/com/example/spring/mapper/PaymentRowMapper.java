87c0394f3c6d1f9160b29540309d19f26d72f001
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2023-01-30 08:54:23 +0100

    Update PaymentRowMapper.java

--- spring/src/main/java/com/example/spring/mapper/PaymentRowMapper.java
+++ spring/src/main/java/com/example/spring/mapper/PaymentRowMapper.java
@@ -13,10 +13,13 @@ public class PaymentRowMapper implements RowMapper<Payment> {
     @Override
     public Payment mapRow(ResultSet rs, int rowNum) throws SQLException, SQLException {
         Payment payment = new Payment();
-        payment.setId(rs.getLong("id"));
-        payment.setEmployeeId(rs.getInt("employee_id"));
-        payment.setAmount(valueOf(rs.getDouble("amount")));
-        payment.setDate(rs.getDate("date").toLocalDate());
+        payment.setId(rs.getInt("id"));
+        payment.setTitle(rs.getString("title"));
+        payment.setDescription(rs.getString("description"));
+        payment.setCategory(rs.getString("category"));
+        //payment.setEmployeeId(rs.getInt("employee_id"));
+        payment.setAmount(rs.getInt("amount"));
+      //  payment.setDate(rs.getDate("date"));
         return payment;
     }
 }
